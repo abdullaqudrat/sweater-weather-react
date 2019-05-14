ba7a56493b074086ae331d205096e474e7a1e5c7
abdullaqudrat <aqudrat630@gmail.com>
2019-05-13 18:50:53 -0600

    adds hardcoded api call for weather
    -working return in console

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import Form from "./components/Form";
 import Weather from "./components/Weather";
 
 class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault()
+    const api_call = await fetch(`https://fierce-crag-36750.herokuapp.com/api/v1/forecast?location=denver,co`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div> 
     );
