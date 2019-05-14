630891949a1d955bfce96ca9b6f8c46ed3f33708
abdullaqudrat <aqudrat630@gmail.com>
2019-05-13 21:03:23 -0600

    Displays search results
    - adds target for city input
    - adds state for results
    - passes state into props for weather component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,39 @@ import Form from "./components/Form";
 import Weather from "./components/Weather";
 
 class App extends React.Component {
+  state = {
+    currentTemp: undefined,
+    city: undefined,
+    currentHumidity: undefined,
+    currentSummary: undefined,
+    error: undefined
+  }
   getWeather = async (e) => {
     e.preventDefault()
-    const api_call = await fetch(`https://fierce-crag-36750.herokuapp.com/api/v1/forecast?location=denver,co`);
-    const data = await api_call.json();
-    console.log(data);
+    const city = e.target.elements.city.value;
+    const api_call = await fetch(`https://fierce-crag-36750.herokuapp.com/api/v1/forecast?location=${city}`);
+    const results = await api_call.json();
+    console.log(results);
+    this.setState({
+      currentTemp: results.data.attributes.current_temp,
+      city: results.data.attributes.location,
+      currentHumidity: results.data.attributes.current_humidity,
+      currentSummary: results.data.attributes.current_summary,
+      error: ""
+    })
   }
   render() {
     return (
       <div>
         <Titles />
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather 
+          currentTemp={this.state.currentTemp}
+          city={this.state.city}
+          currentHumidity={this.state.currentHumidity}
+          currentSummary={this.state.currentSummary}
+          error={this.state.error} 
+        />
       </div> 
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,12 @@ import React from "react";
 class Weather extends React.Component {
   render() {
     return(
-      <div>Weather Results</div>
+      <div>
+        <p>Current Temperature: { this.props.currentTemp }</p>
+        <p>Location: { this.props.city }</p>
+        <p>Current Humdity: { this.props.currentHumidity }</p>
+        <p>Current Summary: { this.props.currentSummary }</p>
+      </div>
     );
   }
 };
