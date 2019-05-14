d454af706560a710cc1b818e1870fb8b4b29b44f
abdullaqudrat <aqudrat630@gmail.com>
2019-05-14 14:27:33 -0600

    Adds navbar component
    -adds navbar shell with logo and signup/login links

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = props => (
+  <header className="navbar">
+    <nav className="navbar__navigation">
+      <div></div>
+      <div className="navbar_title"><a href="/">SW</a></div>
+      <div className="navbar_spacer" />
+      <div className="navbar_navigation-items" >
+        <ul>
+          <li><a href="/">login</a></li>
+          <li><a href="/">signup</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+
+export default Navbar;
