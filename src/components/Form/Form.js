7435a6b6dcd34e9846b47863e87bcdb318338144
abdullaqudrat <aqudrat630@gmail.com>
2019-05-14 23:20:01 -0600

    Adds Form folder
    - moves form component
    - creates form css

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Form.css"
 
 // class-based component, this example doesn't need state and can be a stateless functional component, but using as a reference
 
@@ -6,9 +7,10 @@ class Form extends React.Component {
   render() {
     return(
       <div>
+        <p>Search for a city to get the weather forecast</p>
         <form onSubmit={this.props.getWeather}>
-          <input type="text" name="city" placeholder="Enter city here..."/>
-          <button>Get Forecast</button>
+        <input type="text" name="city" placeholder="Enter city here..."/>
+        <button>Get Forecast</button>
         </form>
       </div>
     );
